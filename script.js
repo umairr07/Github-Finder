a84934293abf5ce9e242178fb1c6311adeae562c
Umair Shaikh <imumairshaikh07@gmail.com>
2024-04-15 19:20:18 +0530

    done

--- script.js
+++ script.js
@@ -24,50 +24,50 @@ btn.addEventListener('click', function () {
                 div.innerHTML = `
                 <div class="main-container mt-10 bg-gray-200 p-5 rounded-lg">
                     <div class="flex">
-                    <img src="${data.avatar_url}" alt="" class="h-28 rounded-full" />
+                    <img src="${data.avatar_url ? data.avatar_url : '-'}" alt="" class="h-28 rounded-full" />
                         <div class="flex justify-between gap-60 mt-8">
                             <div class="ml-10">
-                                <p class="font-bold text-xl">${data.name}</p>
-                                <a class="mt-8" href="${data.html_url}">@${data.login}</a>
+                                <p class="font-bold text-xl">${data.name ? data.name : '-'}</p>
+                                <a class="mt-8" href="${data.html_url}">@${data.login ? data.login : '-'}</a>
                             </div>
                             <div class="inner-right mt-8">
-                                <p>Joined ${data.created_at}</p>
+                                <p>Joined ${data.created_at ? data.created_at : '-'}</p>
                             </div>
                         </div>
                     </div>
 
                     <div class="mt-10 text-lg">
-                        <p>${data.bio}</p>
+                        <p>${data.bio ? data.bio : '-'}</p>
                     </div>
 
                     <div class="account flex justify-between mt-10 bg-white rounded-lg p-3">
                         <div class="repos">
                             <p class="font-bold">Repos</p>
-                            <p>${data.public_repos}</p>
+                            <p>${data.public_repos ? data.public_repos : '-'}</p>
                         </div>
                         <div class="followers">
                             <p class="font-bold">Followers</p>
-                            <p>${data.followers}</p>
+                            <p>${data.followers ? data.followers : '-'}</p>
                         </div>
                         <div class="following">
                             <p class="font-bold">Following</p>
-                            <p>${data.following}</p>
+                            <p>${data.following ? data.following : '-'}</p>
                         </div>
                     </div>
 
                     <div class="mt-10 mb-10">
                         <div >
                             <i class="fas fa-fw fa-map-marked-alt"></i>
-                            <p class="pt-2">${data.location}</p>
+                            <p class="pt-2">${data.location ? data.location : '-'}</p>
                             <i class="fab fa-fw fa-twitter"></i>
-                            <p class="pt-2">${data.twitter_username}</p>
+                            <p class="pt-2">${data.twitter_username ? data.twitter_username : '-'}</p>
                         
                         </div>
                         <div class="">
                             <i class="fas fa-fw fa-link"></i>
-                            <p class="pt-2">${data.blog}</p>
+                            <p class="pt-2">${data.blog ? data.blog : '-'}</p>
                             <i class="fas fa-fw fa-building"></i>
-                            <p class="pt-2">${data.company}</p>
+                            <p class="pt-2">${data.company ? data.company : '-'}</p>
                         
                         </div>
                     </div>
