6d2976d3850f6c3da1048a01fc4842d57778de83
Umair Shaikh <imumairshaikh07@gmail.com>
2024-04-15 16:30:36 +0530

    done

--- script.js
+++ script.js
@@ -22,18 +22,25 @@ btn.addEventListener('click', function () {
                 let div = document.createElement('div');
 
                 div.innerHTML = `
-                <div class="main-container mt-10">
-                    <div class="flex gap-56">
-                        <div class="inner-left flex gap-5">
-                            <img src="${data.avatar_url}" alt="" class="h-20" />
-                            <a class="mt-8" href="${data.html_url}">@${data.login}</a>
-                        </div>
-                        <div class="inner-right mt-8">
-                            <p>Joined ${data.created_at}</p>
+                <div class="main-container mt-10 bg-gray-200 p-5 rounded-lg">
+                    <div class="flex">
+                    <img src="${data.avatar_url}" alt="" class="h-28 rounded-full" />
+                        <div class="flex justify-between gap-60 mt-8">
+                            <div class="ml-10">
+                                <p class="font-bold text-xl">${data.name}</p>
+                                <a class="mt-8" href="${data.html_url}">@${data.login}</a>
+                            </div>
+                            <div class="inner-right mt-8">
+                                <p>Joined ${data.created_at}</p>
+                            </div>
                         </div>
                     </div>
-    
-                    <div class="account flex justify-between mt-10 bg-gray-300 rounded-lg p-3">
+
+                    <div class="mt-10 text-lg">
+                        <p>${data.bio}</p>
+                    </div>
+
+                    <div class="account flex justify-between mt-10 bg-white rounded-lg p-3">
                         <div class="repos">
                             <p class="font-bold">Repos</p>
                             <p>${data.public_repos}</p>
@@ -47,8 +54,27 @@ btn.addEventListener('click', function () {
                             <p>${data.following}</p>
                         </div>
                     </div>
+
+                    <div class="mt-10 mb-10">
+                        <div >
+                            <i class="fas fa-fw fa-map-marked-alt"></i>
+                            <p class="pt-2">${data.location}</p>
+                            <i class="fab fa-fw fa-twitter"></i>
+                            <p class="pt-2">${data.twitter_username}</p>
+                        
+                        </div>
+                        <div class="">
+                            <i class="fas fa-fw fa-link"></i>
+                            <p class="pt-2">${data.blog}</p>
+                            <i class="fas fa-fw fa-building"></i>
+                            <p class="pt-2">${data.company}</p>
+                        
+                        </div>
+                    </div>
+                    </div>
                 </div>
                 `
+
                 info.appendChild(div);
                 ptag.style.display = "none";
             }
